2ace005b0b074dbc9b33d1e0dc98173d6fecc750
mukulietlucknow <mukul.ietlucknow@gmail.com>
2020-03-25 14:28:18 +0530

    Create Inheritence_2.py
    creating class in diffrent manner

---
+++ Inheritence_2.py
@@ -0,0 +1,25 @@
+class Polygon:
+
+    def __init__(self, width , height):
+        self.__width = width
+        self.__height = height
+
+    def set_value(self, width , height):
+        self.__width = width
+        self.__height = height
+
+    def get_height(self):
+        return self.__height
+
+    def get_width(self):
+        return self.__width
+
+
+class Square(Polygon):
+    def area(self):
+        return (self.get_height() * self.get_width())
+
+
+s1 = Square(10,10)
+#s1.set_value(10,10)
+print(s1.area())
