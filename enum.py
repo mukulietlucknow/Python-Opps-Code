842bb30c2c38cd8cb98acc9bc88be5d21391dbd0
mukulietlucknow <mukul.ietlucknow@gmail.com>
2020-03-29 23:34:54 +0530

    collection

---
+++ enum.py
@@ -0,0 +1,20 @@
+from enum import Enum , unique , auto
+
+@unique # for creating the unique enum classes
+class Fruit(Enum):
+    APPLE = 1
+    ORANGE =2
+    TOMATO = 4
+    BANANA = 3
+    PEAR = auto()
+
+def main():
+    print(Fruit.APPLE)
+
+    print(Fruit.APPLE.name)
+    print(Fruit.APPLE.value)
+    print(Fruit.PEAR.value)
+
+
+if __name__ == "__main__":
+    main()
